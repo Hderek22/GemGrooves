36331576ead27abf40c483666e1b960caec35eaf
Derek Hall <Hderek22@icloud.com>
2023-05-07 22:05:33 -0400

    simplified purpose

--- src/pages/OurPurpose.js
+++ src/pages/OurPurpose.js
@@ -29,14 +29,8 @@ function OurPurpose() {
     <div style={centerStyle}>
       <p style={fairPayStyle}>
         
-       GemGrooves provides musicians with an innovative platform to sell their songs using Non-Fungible Token (NFT) technologies. We are revolutionizing the music industry by allowing musicians to earn more for their work while giving fans a unique opportunity to own the rights to their favorite music.
-
-Unlike traditional music platforms, GemGrooves allows musicians to have a direct connection between the artist and their fans. Using GemGrooves, musicians can maintain ownership of their work and receive direct payments for every sale, eliminating the need for intermediaries that take a significant portion of their earnings.
-
-Moreover, GemGrooves provides unique features such as exclusive rights to music, allowing fans to own the rights to a particular song or album, giving them access to special perks such as backstage passes or VIP access to shows. This not only provides fans with a unique experience but also provides artists with a reliable source of income beyond music sales.
-
-With unique features such as exclusive rights to music, GemGrooves is poised to revolutionize the music industry, providing artists with a sustainable and reliable source of income while giving fans a unique experience they won't find anywhere else.
-      </p>
+      Our Purpose is simple. Give the control back to the artist, where it belongs.
+      
       <img src={GemGrooveThumb} alt="GemGroove Thumb" style={logoStyle} />
     </div>
   );
